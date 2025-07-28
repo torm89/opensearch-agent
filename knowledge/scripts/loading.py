35b86f244e0fa1cf13689cf8df7c794ee705fb34
torm <tomaszrycz@gmail.com>
2025-07-28 22:32:17 +0200

    Work in progress 01

--- knowledge/scripts/loading.py
+++ knowledge/scripts/loading.py
@@ -1,5 +1,6 @@
 import json
 import os
+from enum import verify
 from pathlib import Path
 
 import boto3
@@ -58,15 +59,15 @@ def load_docs():
     )
 
     root_path = Path(__file__).parent.parent.parent
-    data_path = root_path / "kownledge" / "data"
+    data_path = root_path / "knowledge" / "data"
 
     chunks = []
     with (data_path / "opensearch_docs_chunks.ndjson" ).open("r", encoding="utf-8") as file:
         for line in file.readlines():
             chunks.append(Document(**json.loads(line)))
 
-    opensearch_url = os.environ.get("OPENSEARCH_URL")
-    opensearch_index_name = "opensearch-agent-staging"
+    opensearch_url = "https://localhost:9200"
+    opensearch_index_name = "opensearch-agent-staging-knowledge-opensearch-docs"
 
     print(f"OpenSearch URL: {opensearch_url}")
     print(f"OpenSearch index name: {opensearch_index_name}")
@@ -78,20 +79,23 @@ def load_docs():
         opensearch_url=opensearch_url,
         index_name=opensearch_index_name,
         embedding_function=embeddings,
-        http_auth=aws_auth,
-        use_ssl=True,
-        verify_certs=True,
+        http_auth=('admin', "M6TrRmXa!"),
+        use_ssl=False,
+        verify_certs=False,
+        # http_auth=aws_auth,
+        # use_ssl=True,
+        # verify_certs=True,
         connection_class=RequestsHttpConnection,
         timeout=300
     )
 
-    batch_size = 500
+    batch_size = 250
     for i in tqdm.tqdm(range(0, len(chunks), batch_size)):
         batch = chunks[i:i + batch_size]
         vector_store.add_documents(batch)
 
 
 if __name__ == "__main__":
-    # load_docs()
+    load_docs()
     # load_examples()
-    pass
+    # pass
